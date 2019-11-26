2a726e03220f2009202f8c7eecd839c7c40c0ba0
User <https://github.com/karno786/Sqlite_git_test>
2019-11-27 02:54:22 +0600

    Fragment Added.

---
+++ app/src/main/java/com/teamblank/tourmate/fragments/WalletFragment.java
@@ -0,0 +1,35 @@
+package com.teamblank.tourmate.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamblank.tourmate.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WalletFragment extends Fragment {
+
+
+    public WalletFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        return view;
+    }
+
+}
