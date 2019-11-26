ac2770fa99e997e79c3af3c64bd749a4c334b7be
User <https://github.com/karno786/Sqlite_git_test>
2019-11-27 02:53:33 +0600

    Fragment Added.

--- app/src/main/java/com/teamblank/tourmate/activity/MainActivity.java
+++ app/src/main/java/com/teamblank/tourmate/activity/MainActivity.java
@@ -1,16 +1,74 @@
 package com.teamblank.tourmate.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teamblank.tourmate.R;
+import com.teamblank.tourmate.fragments.MemoriesFragment;
+import com.teamblank.tourmate.fragments.TripFragment;
+import com.teamblank.tourmate.fragments.WalletFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView toolbarTV;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        defaultFragment();
+
+        setBotoomNavigation();
+    }
+
+    private void defaultFragment() {
+        replaceFragment(new TripFragment());
+    }
+
+    private void setBotoomNavigation() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_trips:
+                        toolbarTV.setText("Trips");
+                        replaceFragment(new TripFragment());
+                        return true;
+                    case R.id.nav_memories:
+                        toolbarTV.setText("Memories");
+                        replaceFragment(new MemoriesFragment());
+                        return true;
+                    case R.id.nav_wallet:
+                        toolbarTV.setText("Wallet");
+                        replaceFragment(new WalletFragment());
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void init() {
+        toolbarTV = findViewById(R.id.toolbarTV);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
     }
 }
---
+++ app/src/main/java/com/teamblank/tourmate/model_class/Trip.java
@@ -0,0 +1,51 @@
+package com.teamblank.tourmate.model_class;
+
+public class Trip {
+    private String userid,triptitle,startlocation,triplocation,startdate,enddate,tripdetails,tripbudget;
+
+    public Trip() {
+    }
+
+    public Trip(String userid, String triptitle, String startlocation, String triplocation, String startdate, String enddate, String tripdetails, String tripbudget) {
+        this.userid = userid;
+        this.triptitle = triptitle;
+        this.startlocation = startlocation;
+        this.triplocation = triplocation;
+        this.startdate = startdate;
+        this.enddate = enddate;
+        this.tripdetails = tripdetails;
+        this.tripbudget = tripbudget;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getTriptitle() {
+        return triptitle;
+    }
+
+    public String getStartlocation() {
+        return startlocation;
+    }
+
+    public String getTriplocation() {
+        return triplocation;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public String getTripdetails() {
+        return tripdetails;
+    }
+
+    public String getTripbudget() {
+        return tripbudget;
+    }
+}
